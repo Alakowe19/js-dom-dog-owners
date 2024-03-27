8e3a01f468e1c1834d92d915ff680e16b3d952ba
Oluwagbemi Abiodun <oluwagbemiiabiodun@gmail.com>
2024-03-27 19:53:19 +0000

    Not done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,55 @@
-console.log(data);
+document.addEventListener('DOMContentLoaded', function () {
+    const header = document.querySelector('header')
+    const dogsList = document.querySelector('.dogs-list')
+    const dogsListButton = document.querySelector('.dogs-list_button')
+    const main = document.querySelector('.main')
+    const mainDogSection = document.querySelector('.main__dog-section')
+  
+    function renderDogList() {
+      for (let i = 0; i < data.length; i++) {
+        const dogJs = data[i]
+        const listItem = document.createElement('li')
+        listItem.classList.add('dogs-list__items')
+        listItem.textContent = dogJs.name
+        listItem.addEventListener('click', function () {
+          renderMainCard(dogJs)
+        })
+        dogsList.appendChild(listItem)
+      }
+    }
+  
+    function renderMainCard(dogJs) {
+      const mainCardContent = document.createElement('div')
+      mainCardContent.classList.add('main-card-content')
+      mainDogSection.innerHTML = '' 
+      mainDogSection.appendChild(mainCardContent)
+  
+       //Rendering the dogname
+       const dogName = document.createElement('h2')
+       dogName.textContent = dogJs.name
+       mainCardContent.appendChild(dogName)
 
-// WRITE YOUR CODE BELOW!
+      //Rendering the image
+      const dogImage = document.createElement('img')
+      dogImage.classList.add('main-card__image')
+      dogImage.setAttribute('alt', dogJs.name)
+      dogImage.setAttribute('src', dogJs.image)
+      mainCardContent.appendChild(dogImage)
+  
+      //Rendering the Dog info
+      const dogInfo = document.createElement('p')
+      dogInfo.classList.add('main-card__info')
+      mainCardContent.appendChild(dogInfo)
+  
+      const dogBio = document.createElement('p')
+      dogBio.textContent = "Bio: " + dogJs.bio
+      dogInfo.appendChild(dogBio)
+  
+      const isGoodDog = document.createElement('p')
+      isGoodDog.textContent = "Is Good Dog: " + (dogJs.isGoodDog ? 'Yes' : 'No')
+      dogInfo.appendChild(isGoodDog)
+    }
+  
+    renderDogList()
+  })
+  
